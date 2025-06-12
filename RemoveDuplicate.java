8ff55c5cde345ffa8ead7c51bb7935d99229d6d1
aloksingh72 <aloksingh36757@gmail.com>
2025-06-12 16:27:14 +0530

    DSA_Question_sheet

--- RemoveDuplicate.java
+++ RemoveDuplicate.java
@@ -2,11 +2,13 @@ class RemoveDuplicate{
 	public static void main(String[] args){
 
 	int[] arr = {1,1,2,2,2,3,3,4};
+	//method calling
 	int k = removeduplicate(arr);
 	for(int i =0;i<k;i++){
 		System.out.print(arr[i]+" ");
 	}
 }	
+	//method declaration
 	public static int removeduplicate(int[] arr){
 	int i =1;
 	for(int j=1;j<arr.length;j++){
