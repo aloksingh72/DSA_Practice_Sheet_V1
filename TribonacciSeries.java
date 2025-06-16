6c8fc6031c92c3f103b0ada59c9c05ba76d8d3fd
aloksingh72 <aloksingh36757@gmail.com>
2025-06-16 12:54:02 +0530

    Tribonacci series

---
+++ TribonacciSeries.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+class TribonacciSeries {
+    public static void main(String[] args) {
+        System.out.println("Enter the number of terms:-");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int x =0,y=1,z=1;
+        System.out.println("Tribonacci series:-");
+        System.out.print(x+" ");
+        System.out.print(y+" ");
+        System.out.print(z+" ");
+        int w = x+y+z;
+        for(int i =4;i<=n;i++){
+            x= y;
+            y=z;
+            z=w;
+            w=x+y+z;
+            System.out.print(w+" ");
+        }
+        System.out.println();
+    }
+}
+
+
