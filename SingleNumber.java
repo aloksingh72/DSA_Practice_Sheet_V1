c133779e8254cf7ec99208b0d9704febd9461535
aloksingh72 <aloksingh36757@gmail.com>
2025-06-18 16:56:10 +0530

    Single number

--- SingleNumber.java
+++ SingleNumber.java
@@ -1,8 +1,12 @@
+//Find the number that appears once, and the other numbers twice
+//Input Format: arr[] = {4,1,2,1,2}
+//Output_Result: 4
+//Explanation: In this array, only element 4 appear once and the other elements appear twice. So, 4 is the answer.
 class SingleNumber{
 
 	public static void main(String[] args){
 	
-	int[] arr= {4,1,2,2,1};
+	int[] arr= {1,2,1};
 	
 	
 	int res = singlenumber(arr);
@@ -10,21 +14,24 @@ class SingleNumber{
 	}
 	
 	public static int singlenumber(int[] arr){
-	int count =0,max_count =0;
 	
-	for(int i =0;i<arr.length;i++){
-			if(arr[i] ==1){
-				count++;	
-			}
-			else{
-			count=0;
-			}
+	int element = 0;
+	for(int i=0;i<arr.length;i++){
+		int count =0;
+		element = arr[i];
+		//find the occurence
+		for(int j=0;j<arr.length;j++){
+			if(arr[j] == element){
 			
-			if(count>max_count){
-			max_count = count;
+				count++;
+				}
 			}
+		if(count ==1){
+			return element;
 		}
-		return max_count;
+		
+		}
+		return -1;
 	}
 
 }
