0d0c1a3bcd1854c7f747751c411042a88e624d6b
Alok Singh <112793361+aloksingh72@users.noreply.github.com>
2025-07-27 22:27:25 +0530

    Update UnionOfArray.java

--- UnionOfArray.java
+++ UnionOfArray.java
@@ -3,7 +3,8 @@ class UnionOfArray {
     public static void main(String[] args) {
    int[] arr1 = {1,2,3,4,9,9};
    int[] arr2 = {2,2,3,4,5,6,7,8,9};
-   
+
+        //method calling
    unionofarray(arr1,arr2);
  
 }
