10d18cc581b38b86a116a8186bddfa6a7233a0dc
Alok Singh <aloksingh36757@gmail.com>
2025-08-27 11:38:24 +0530

    program1 completed

--- ArraysPythonSheet/program1.py
+++ ArraysPythonSheet/program1.py
@@ -1,8 +1,28 @@
 # Print biggest element , smallest elements and their difference from the given array.
 
 
-print("hello")
-a=12
-b=12
-sum = a+b
-print("The sum of two number",sum)
+# print("hello")
+# a=12
+# b=12
+# sum = a+b
+# print("The sum of two number",sum)
+
+def biggestSmallestDifference(A):
+    biggest = float('-inf')
+    smallest = float('inf')
+
+    for num in A:
+        if(num >biggest):
+            biggest = num
+        if(num <smallest):
+            smallest = num
+    
+    diff = biggest-smallest
+    print("the biggest number is ",biggest)
+    print("the smallest number is ",smallest)
+    print("the difference among the number is ",diff)
+    return
+
+if __name__ == "__main__":
+    A=[1,5,9,7,6,3]
+    biggestSmallestDifference(A)
