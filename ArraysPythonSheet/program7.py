530f7ee0a381107700c685faf7f2dba17531471a
Alok Singh <aloksingh36757@gmail.com>
2025-10-08 12:24:22 +0530

    program8

---
+++ ArraysPythonSheet/program7.py
@@ -0,0 +1,29 @@
+# WAPP to print and count all the palindrome
+# number elements from array.
+# Plaindrome:- Same backward as forward
+
+def countPlaindrome(A):
+    count =0
+    for num in A:
+        orgNum =  num
+        
+        rev =0
+        while(num>0):
+            rem  = num%10
+            rev = rev*10 +rem
+            num = num//10
+        
+        if rev == orgNum:
+            print(orgNum,end=" ")
+            count +=1
+    print("\ntotal palindrome numbers",count,end=" ")
+         
+
+
+
+
+
+if __name__=="__main__":
+    A=[121,544,999,66,83,707]
+    countPlaindrome(A)
+
---
+++ ArraysPythonSheet/program8.py
@@ -0,0 +1,1 @@
+# WAPP to store first n prime numbers into array
