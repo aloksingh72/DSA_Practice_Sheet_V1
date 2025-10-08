cb303c493b6ea8b54ffd4e7279a5aadb648f844f
Alok Singh <aloksingh36757@gmail.com>
2025-10-08 17:20:56 +0530

    question 8 done

--- ArraysPythonSheet/program8.py
+++ ArraysPythonSheet/program8.py
@@ -1,1 +1,23 @@
 # WAPP to store first n prime numbers into array
+
+def storePrime(n):
+    primelist=[]
+    num=2
+    while len(primelist)<n:
+        isPrime = True
+        for i in range(2,int(num*0.5)+1):
+            if num%i ==0:
+                isPrime = False
+                break
+        if isPrime:
+            primelist.append(num)
+        num +=1
+    
+    print("First", n, "prime numbers are:",primelist)
+
+
+
+
+
+if __name__=="__main__":
+    storePrime(5)
