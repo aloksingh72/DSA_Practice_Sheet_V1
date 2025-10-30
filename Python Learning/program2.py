8a78160cd8773a38de75abaa4c6a20cb96b64a67
Alok Singh <aloksingh36757@gmail.com>
2025-10-30 18:28:16 +0530

    inseert eztend method

--- Python Learning/program2.py
+++ Python Learning/program2.py
@@ -59,7 +59,7 @@ print("------------------------------")
 my_list.insert(1,"aloksingh")
 print(my_list)
 
-
+# ------extend and insert method----------
 # Creating two lists
 list3 = [1, 2, 3]
 list4 = [4, 5, 6]
@@ -69,3 +69,5 @@ list3.insert(1, list4)
 
 # Displaying the updated list1
 print(list3)  # Output: [1, [4, 5, 6], 2, 3]
+
+
