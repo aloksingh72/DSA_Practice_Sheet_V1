dea199ce3468bba4841229ca8f57d9c251bcab23
Alok Singh <aloksingh36757@gmail.com>
2025-11-03 15:04:09 +0530

    new updated functions

--- Python Learning/Day4.py
+++ Python Learning/Day4.py
@@ -5,4 +5,24 @@ def add_numbers(a,b):
     return result
 
 
-add_numbers(3,6)
+ans = add_numbers(3,6)
+print("the sum of two numners",ans)
+
+
+# default values in function 
+
+def greeting(name,greet = "hello"):
+    print(f"{greet},{name} !")
+
+
+# function calling
+greeting("Alison")
+
+
+# positonal and keywords arguments
+
+def print_args(*args,**kwargs):
+    print("Postional requirement",args)
+    print("keywords arguments",kwargs)
+
+print_args(1, 2, 3, name="Alice", age=30)
