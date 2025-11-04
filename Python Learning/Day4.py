9b1ae37f56365178dc5c3b0f4d4c8c916bfc08c2
Alok Singh <aloksingh36757@gmail.com>
2025-11-04 15:55:40 +0530

    decorators functions

--- Python Learning/Day4.py
+++ Python Learning/Day4.py
@@ -79,4 +79,62 @@ area = calculate_area(5,6)
 print(calculate_area.__doc__)
 print("the area of  the rectangle",area)
 
-print("----------------------------------------------------")
+print("-------------------------------------------------")
+
+def outer_function(x):
+    # Inner function is defined within the scope of outer_function
+    def inner_function(y):
+      
+        return x + y
+    # Return the inner function, creating a closure
+    return inner_function
+
+# Create closures with different values of x
+closure_1 = outer_function(10)
+closure_2 = outer_function(5)
+
+# Call the closures with different values of y
+result_1 = closure_1(3)
+result_2 = closure_2(3)
+
+# Print the results
+print(result_1)  # Output: 13
+print(result_2)  # Output: 8
+
+
+
+print("-----------------------------------------")
+
+numbers= [1,2,3,4,5]
+squared = list(map(lambda x:x**2,numbers))
+
+
+even_numbers = list(filter(lambda x:x%2 ==0,numbers))
+print("Squared numbers:-",squared)
+print("Filter  even numbers",even_numbers)
+
+print("----------------------------------------")
+
+
+# decorators
+import time
+def my_decorators(func):
+    def wrapper():
+        start  =  time.time()
+        print("before the function calling")
+        func()
+        print("after the function calling")
+        end= time.time()
+        print("time taken",end-start,"seconds")
+    return wrapper
+
+@my_decorators
+def say_hello():
+    print("Hello!")
+    time.sleep(3)
+
+say_hello()
+
+print("--------------------------------------------")
+
+
