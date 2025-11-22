ff2ecfe3fe2438eeb2af0a5fa41b889db2193e2e
Alok Singh <aloks@triazinesoft.com>
2025-11-22 23:25:55 +0530

    recursion questions

--- Recursion/Assign18.py
+++ Recursion/Assign18.py
@@ -7,5 +7,39 @@ def printNaturalNumber(n):
         printNaturalNumber(n-1)
         print(n,end=" ")
 
-printNaturalNumber(5)
+def printNReverse(n):
+    if n>0:
+        print(n,end=" ")
+        printNReverse(n-1)
+
+def printOddNaturalNo(n):
+    if n >0:
+        printOddNaturalNo(n-1)
+        print(2*n-1,end=" ")
+
+def evenNaturalNo(n):
+    if n>0:
+        evenNaturalNo(n-1)
+        print(2*n,end=" ")
+def oddNaturalNumberRev(n):
+    if n>0:
+        print(2*n-1,end = " ")
+        oddNaturalNumberRev(n-1)
+def evenNaturalNoRev(n):
+    if n>0:
+        print(2*n,end=" ")
+        evenNaturalNoRev(n-1)
+
+printNaturalNumber(10)
+print()
+printNReverse(10)
+print()
+printOddNaturalNo(10)
+print()
+evenNaturalNo(10)
+print()
+oddNaturalNumberRev(10)
+print()
+evenNaturalNoRev(10)
+
 
