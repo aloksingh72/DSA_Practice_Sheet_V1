768e6deb5cf9e5a53ff3c6649dd464cac9a0a070
Alok Singh <aloks@triazinesoft.com>
2025-11-23 12:52:44 +0530

    added comment

--- Recursion/Assign19.py
+++ Recursion/Assign19.py
@@ -2,28 +2,29 @@
 
 # Assignment:- 19
 # sum of n natural number through Recursion
-
 def sumN(n):
     if n == 0:
         return 0
     return n+sumN(n-1)
 
+# sum of N odd natural number
 def sumNodd(n):
     if n ==0:
         return 0
     return 2*n-1 + sumNodd(n-1)
 
+# sum of N even natural number
 def sumNEven(n):
     if n==0:
         return 0
     return 2*n +sumNEven(n-1)
 
-
+# calculate the factorial of number
 def fact(n):
     if n==0:
         return 1
     return n*fact(n-1)
-
+# sum of squares of first n natural numbers
 def sumNSquare(n):
     if n ==0:
         return 0
